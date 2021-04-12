aed7d85bd64210796345f3f48d26649c678efbbf
Emil Hristov <emodavitkov@abv.bg>
2021-04-12 23:30:07 +0300

    ready

---
+++ AverageStudentGrades/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
